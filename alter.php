b00f1dd336343f84c8b14777306a8bd816b87bf6
saheedibrahim <saolabram@gmail.com>
2023-04-17 04:51:08 +0100

    little change

--- alter.php
+++ alter.php
@@ -7,8 +7,8 @@ $dbname = "goods_delivery";
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "DELETE FROM Person 
-    -- MODIFY COLUMN PhoneNo VARCHAR(12) NOT NULL";
+    $sql = "DELETE FROM Person";
+    // -- MODIFY COLUMN PhoneNo VARCHAR(12) NOT NULL";
     $conn->exec($sql);
     echo "Column edited successfully";
 } catch(PDOException $e){
